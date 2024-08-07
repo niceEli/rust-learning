b52a688a58200bd9a37756ce17066b0ec4f0cbba
Eli <78963198+niceEli@users.noreply.github.com>
2024-08-06 22:12:45 -0400

    chore: init

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,11 @@
+// https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+
+use std::io;
+
+fn main() {
+    println!("Guess The Number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+}
